a9b9868a67a995a785d5a3f1bd6317cb71944eb5
minecraftchest1 <minecraftchest1@outlook.com>
2023-01-26 10:53:25 -0600

    Start table generation.

--- readmeGen.sh
+++ readmeGen.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-sed '/^#/d' < data.csv 
+# Field ID's count from 1
+#ID_COL=1
+#NAME_COL=2
+#DATE_COL=3
+
+genTable()
+{
+	IFS=','
+	sed '/^#/d' data.csv | while read -t 0.1 -r -a line
+	do
+		# Print table entries
+		printf '%s | ' "${#line[0]}" #Print ID
+		printf '%s | ' "${#line[1]}" #Print ID
+		printf '%s | ' "${#line[2]}" #Print ID
+		printf '%s | ' "${#line[3]}" #Print ID
+		printf '\n'
+	done
+}
+
+genTable
