eea1f53cd79d3c04eeb958d7a6421e2ade8c1bde
minecraftchest1 <minecraftchest1@outlook.com>
2023-01-26 21:07:08 -0600

    Finish table generation

--- readmeGen.sh
+++ readmeGen.sh
@@ -7,15 +7,30 @@
 
 genTable()
 {
+	x=0
 	IFS=','
 	sed '/^#/d' data.csv | while read -t 0.1 -r -a line
 	do
 		# Print table entries
-		printf '%s | ' "${#line[0]}" #Print ID
-		printf '%s | ' "${#line[1]}" #Print ID
-		printf '%s | ' "${#line[2]}" #Print ID
-		printf '%s | ' "${#line[3]}" #Print ID
+		printf '%s | ' "${line[0]}" #Print ID
+		printf '%s | ' "${line[1]}" #Print Product Name
+		printf '%s | ' "${line[2]}" #Test Date
+		printf '%s | ' "${line[3]}" #CPU Test 1 Single
+		printf '%s | ' "${line[4]}" #Cpu Test 1 Multi
+		printf '%s | ' "${line[5]}" #Cpu Test 2
+		printf '%s | ' "${line[6]}" #Gpu Test 1
+		printf '%s | ' "${line[7]}" #Gpu Test 2
+		printf '%s | ' "${line[8]}" #Memory Test 1
+		printf '%s | ' "${line[9]}" #Memory Test 2
+		printf '%s | ' "${line[10]}" #Network Upload Test 1
+		printf '%s | ' "${line[11]}" #Network Upload Test 2
+		printf '%s | ' "${line[12]}" #Network Down Test 1
+		printf '%s | ' "${line[13]}" #Network Down Test 2
+		printf '%s | ' "${line[14]}" #Network Bidiretional Test 1
+		printf '%s | ' "${line[15]}" #Network Bidiretional Test 2
 		printf '\n'
+		[  $x -ne 1 ] && printf '%s\n ' '----------'
+		x=1
 	done
 }
 
